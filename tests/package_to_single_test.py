818fb80ec1d35bd7fa2d7b75ddfbb35636e42042
Jens Kutilek <webmail@kutilek.de>
2022-08-22 11:56:41 +0200

    Add basic testing

---
+++ tests/package_to_single_test.py
@@ -0,0 +1,36 @@
+import pytest
+import unittest
+
+from glyphspkg.package_to_single import package_to_single
+from pathlib import Path
+
+
+def get_mono_path():
+    return (
+        Path(__file__).parent / "data" / "JetBrainsMono-Italic.glyphspackage"
+    )
+
+
+def get_tmp_path():
+    return Path(__file__).parent / "data" / "tmp"
+
+
+class PackageToSingleText(unittest.TestCase):
+    def test_with_output_dir(self):
+        out_dir = get_tmp_path()
+        package_to_single(get_mono_path(), out_dir)
+        out_file = out_dir / "JetBrainsMono-Italic.glyphs"
+        assert out_file.is_file()
+
+    def test_with_output_file(self):
+        out_file = get_tmp_path() / "custom.glyphs"
+        package_to_single(get_mono_path(), out_file)
+        assert out_file.is_file()
+
+    @classmethod
+    def tearDownClass(cls):
+        for entry in get_tmp_path().iterdir():
+            if entry.is_dir():
+                entry.rmdir()
+            else:
+                entry.unlink()
