e51095417b29f11f5bcaedef3c2966fc20eb838f
Jens Kutilek <webmail@kutilek.de>
2024-05-09 13:26:42 +0200

    Move build_output_file_path to separate file

--- Lib/glyphspkg/package_to_single.py
+++ Lib/glyphspkg/package_to_single.py
@@ -1,6 +1,7 @@
 import logging
 
 from glyphspkg.filenames import userNameToFileName
+from glyphspkg.paths import build_output_file_path
 from glyphspkg.plist import parse_plist_from_path, save_to_plist_path
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Union
@@ -48,24 +49,6 @@ def package_to_single(
 
     logger.info(f"Saving: {output_file_path}")
     save_to_plist_path(glyphs_file, output_file_path)
-    return output_file_path
-
-
-def build_output_file_path(
-    input_path: Path, output_path: Optional[Path] = None
-) -> Path:
-    file_name = input_path.with_suffix(".glyphs").name
-    if output_path is None:
-        # No path was specified, save next to original file
-        output_file_path = input_path.parent / file_name
-    elif output_path.is_dir():
-        # Output directory was specified, save to original file name with
-        # changed suffix in new dir
-        output_file_path = output_path / file_name
-    else:
-        # Full path with file name was specified, save there
-        output_file_path = output_path
-
     return output_file_path
 
 
---
+++ Lib/glyphspkg/paths.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from typing import Optional
+
+
+def build_output_file_path(
+    input_path: Path, output_path: Optional[Path] = None, target_suffix: str = ".glyphs"
+) -> Path:
+    file_name = input_path.with_suffix(target_suffix).name
+    if output_path is None:
+        # No path was specified, save next to original file
+        output_file_path = input_path.parent / file_name
+    elif output_path.is_dir():
+        # Output directory was specified, save to original file name with
+        # changed suffix in new dir
+        output_file_path = output_path / file_name
+    else:
+        # Full path with file name was specified, save there
+        output_file_path = output_path
+
+    return output_file_path
